9113070c664127770a79c189208e2a0cbae2d6da
tanveerprottoy <tanveer.prottoy@gmail.com>
2022-11-26 02:32:02 +0600

    init

---
+++ src/Semaphore.java
@@ -0,0 +1,58 @@
+import java.util.concurrent.Semaphore;
+
+public class Semaphore {
+    // max 4
+    static Semaphore semaphore = new Semaphore(4);
+
+    static class SemaphoreThread extends Thread {
+
+        String name = "";
+
+        SemaphoreThread(String name) {
+            this.name = name;
+        }
+
+        public void run() {
+
+            try {
+
+                System.out.println(name + " : acquiring lock...");
+                System.out.println(name + " : available Semaphore permits now: "
+                        + semaphore.availablePermits());
+
+                semaphore.acquire();
+                System.out.println(name + " : got the permit!");
+
+                try {
+
+                    for (int i = 1; i <= 5; i++) {
+
+                        System.out.println(name + " : is performing operation " + i
+                                + ", available Semaphore permits : "
+                                + semaphore.availablePermits());
+
+                        // sleep 1 second
+                        Thread.sleep(1000);
+
+                    }
+
+                } finally {
+
+                    // calling release() after a successful acquire()
+                    System.out.println(name + " : releasing lock...");
+                    semaphore.release();
+                    System.out.println(name + " : available Semaphore permits now: "
+                            + semaphore.availablePermits());
+
+                }
+
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+
+            }
+
+        }
+
+    }
+}
